999f3f33987f8ad8ca16ae45a8218c1e83570a97
shabeen <shabeennn@gmail.com>
2022-01-07 16:34:38 +0530

    few tweaks and new splash scr

---
+++ lib/Screens/main_login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class main_login extends StatelessWidget {
+  const main_login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
