148a6069dbe7aa49f3ea5b959041a448d556262c
fadhil rafeeq <fadhilrafeeq@gmail.com>
2022-01-10 23:06:39 +0530

    pub changes

--- lib/Utilities/kConstants.dart
+++ lib/Utilities/kConstants.dart
@@ -1,38 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 import 'package:yenedify/Screens/splash_screen.dart';
 
 class kButton extends StatelessWidget {
-  const kButton({required this.label,required this.onpress});
+  const kButton({required this.label, required this.onpress});
   final String label;
   final void Function() onpress;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        child:
-        Container(
+        child: Container(
           alignment: Alignment.center,
-
           height: 30,
           width: 100,
           decoration: BoxDecoration(
             color: Color(0xBA01979F),
             borderRadius: BorderRadius.circular(5),
           ),
-          child:
-          Text(
+          child: Text(
             'LOGIN',
-            style: GoogleFonts.poppins(
-                color: Colors.white54,
-                fontSize: 16
-            ),
+            style: GoogleFonts.poppins(color: Colors.white54, fontSize: 16),
           ),
-
         ),
         onTap: () {
           onpress;
-        }
-    );
+        });
   }
 }
