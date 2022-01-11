6b30437b480bb510454af4846cfdb7c1f48835b8
shabeen <shabeennn@gmail.com>
2022-01-11 21:53:30 +0530

    Keyboard renderflex fixed + google-services.json placement error resolved + kotlin version and minsdk version tweaked

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -18,10 +18,10 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _EmailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  // final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  // final TextEditingController _EmailController = TextEditingController();
+  // final TextEditingController _passwordController = TextEditingController();
+  // final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   @override
 
@@ -40,156 +40,157 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+          // resizeToAvoidBottomInset : false,
           backgroundColor: Colors.black,
           body: Column(
             children: [
               Expanded(
-                flex: 6,
+                flex: 5,
                 child:
                     //   Top part
                     Container(
                         // color: Colors.red,
                         ),
               ),
+
               Expanded(
                 flex: 6,
-                child: Container(
+                child:
+                    Container(
                   // Middle part
                   // color: Colors.blue,
-                  child: Form(
-                    key: _formKey,
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(53, 0, 53, 53),
-                      child: Column(
-                        // mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Text('LOGIN',
-                                textAlign: TextAlign.start,
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Poppins',
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 35,
-                                    letterSpacing: 2)),
+                            child: Form(
+                    // key: _formKey,
+                            child: Padding(
+                            padding: EdgeInsets.fromLTRB(53, 0, 53, 53),
+                              child: SingleChildScrollView(
+                              physics: NeverScrollableScrollPhysics(),
+                                child: Container(
+                                  child: Column(
+                                  children: <Widget>[
+                                      Align(
+                                      alignment: Alignment.topLeft,
+                                        child: Text('LOGIN',
+                                              textAlign: TextAlign.start,
+                                              style: TextStyle(
+                                              color: Colors.white,
+                                              fontFamily: 'Poppins',
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 35,
+                                              letterSpacing: 2)),
+                                            ),
+
+                                  SizedBox(
+                                            height: 9,
+                                          ),
+
+                                    Column(
+                                      children: [
+                                        Column(
+                                        children: [
+
+                                          TextFormField(
+                                            style: TextStyle(
+                                            color: Colors.white54,
+                                         ),
+                                            keyboardType: TextInputType.number,
+                                            // controller: _EmailController,
+                                            cursorColor: Colors.blueGrey,
+                                            decoration: InputDecoration(
+                                            filled: true,
+                                            fillColor: Color(0xFF171717),
+                                            focusColor: Colors.white70,
+                                            border: OutlineInputBorder(
+                                            borderRadius: BorderRadius.circular(8),
+                                            borderSide: BorderSide.none,
+                                              ),
+                                            // labelText: 'Campus ID',
+                                            hintText: 'Please Enter your Campus ID',
+                                            hintStyle: TextStyle(
+                                            color: Colors.white12,
+                                          ),
+                                        ),
+                                            validator: (value) {
+                                              if (value == null) {
+                                                 return "Required";}
+                                              else if (value.length > 5 ||
+                                                  value.length < 5) {
+                                                 return "Campus ID must be 5 digits";
+                                           } else
+                                                 return null;
+                                            },
+                                          ),
+
+                                          Padding(padding: EdgeInsets.all(10)),
+                                            TextFormField(
+                                              style: TextStyle(
+                                               color: Colors.white54,
+                                              ),
+                                           // controller: _passwordController,
+                                              cursorColor: Colors.blueGrey,
+                                              decoration: InputDecoration(
+                                              filled: true,
+                                              fillColor: Color(0xFF171717),
+                                             border: OutlineInputBorder(
+                                              borderRadius: BorderRadius.circular(5),
+                                               borderSide: BorderSide.none,
+                                               ),
+                                              // labelText: 'Password',
+                                                hintText: 'Please Enter your Password',
+                                                hintStyle: TextStyle(
+                                                color: Colors.white12,
+                                               ),
+                                              ),
+                                              validator: (value) {
+                                             if (value == null) {
+                                               return "Required";
+                                          } else
+                                              return null;},
+                                            ),
+                                         ]),
+
+
+                                        SizedBox(
+                                              height: 20,
+                                            ),
+
+                                        kButton(
+                                            label: "LOGIN",
+                                            onpress: ()=> print("hit"),
+                                          ),
+                                        ]),
+
+
+                                    // FADHIL defined code for reference
+
+                              // async {
+                              //   if (_formKey.currentState!.validate()) {
+                              //     setState(() {
+                              //       // _signInWithIdAndPassword();
+                              //     }
+                              //     );
+                              //   }
+                              // },
+                            ],
                           ),
-
-                          SizedBox(
-                            height: 9,
-                          ),
-
-                          TextFormField(
-                              keyboardType: TextInputType.number,
-                              controller: _EmailController,
-                              cursorColor: Colors.blueGrey,
-                              decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Color(0xFF101010),
-                                focusColor: Colors.white70,
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                  borderSide: BorderSide.none,
-                                ),
-                                // labelText: 'Campus ID',
-                                hintText: 'Please Enter your Campus ID',
-                                hintStyle: TextStyle(
-                                  color: Colors.white10,
-                                ),
-                              ),
-                              validator: (value) {
-                                if (value == null) {
-                                  return "Required";
-                                } else if (value.length > 5 ||
-                                    value.length < 5) {
-                                  return "Campus ID must be 5 digits";
-                                } else
-                                  return null;
-                              }),
-                          Padding(padding: EdgeInsets.all(10)),
-                          TextFormField(
-                              controller: _passwordController,
-                              cursorColor: Colors.blueGrey,
-                              decoration: InputDecoration(
-                                  filled: true,
-                                  fillColor: Color(0xFF101010),
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(5),
-                                    borderSide: BorderSide.none,
-                                  ),
-                                  // labelText: 'Password',
-                                  hintText: 'Please Enter your Password',
-                                  hintStyle: TextStyle(
-                                    color: Colors.white10,
-                                  )),
-                              validator: (value) {
-                                if (value == null) {
-                                  return "Required";
-                                } else
-                                  return null;
-                              }),
-
-                          SizedBox(
-                            height: 20,
-                          ),
-
-                          kButton(
-                            label: "LOGIN",
-                            onpress: () => print("hit"),
-                          )
-
-                          // FADHIL defined code for reference
-
-                          // GestureDetector(
-                          //
-                          //   child:
-                          //   Container(
-                          //     alignment: Alignment.center,
-                          //
-                          //     height: 30,
-                          //     width: 100,
-                          //     decoration: BoxDecoration(
-                          //       color: Color(0xBA01979F),
-                          //       borderRadius: BorderRadius.circular(5),
-                          //     ),
-                          //     child:
-                          //     Text(
-                          //       'LOGIN',
-                          //       style: GoogleFonts.poppins(
-                          //         color: Colors.white54,
-                          //         fontSize: 16
-                          //       ),
-                          //     ),
-                          //
-                          //   ),
-                          //   onTap: () {
-                          //     print("hit");
-                          //   }
-                          //   ),
-
-                          // async {
-                          //   if (_formKey.currentState!.validate()) {
-                          //     setState(() {
-                          //       // _signInWithIdAndPassword();
-                          //     }
-                          //     );
-                          //   }
-                          // },
-                        ],
+                        ),
                       ),
                     ),
                   ),
                 ),
+
               ),
               Expanded(
                 flex: 1,
                 child: Container(
+
                     //bottom space
                     // color: Colors.green,
                     ),
               ),
-            ],
-          )),
+              
+            ],)
+      ),
     );
   }
 
@@ -211,22 +212,22 @@ class _LoginState extends State<Login> {
   //   }
   // }
 
-  _signInWithEmailAndPassword() async {
-    try {
-      final user = (await _firebaseAuth.signInWithEmailAndPassword(
-          email: _EmailController.text.trim(),
-          password: _passwordController.text.trim()));
-      if (user != null) {
-        void setState() {
-          Fluttertoast.showToast(msg: "Signed In Sucessfully");
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => HomePage()),
-          );
-        }
-      }
-    } catch (e) {
-      Fluttertoast.showToast(msg: e.toString());
-    }
-  }
+  // _signInWithEmailAndPassword() async {
+  //   try {
+  //     final user = (await _firebaseAuth.signInWithEmailAndPassword(
+  //         email: _EmailController.text.trim(),
+  //         password: _passwordController.text.trim()));
+  //     if (user != null) {
+  //       void setState() {
+  //         Fluttertoast.showToast(msg: "Signed In Sucessfully");
+  //         Navigator.pushReplacement(
+  //           context,
+  //           MaterialPageRoute(builder: (context) => HomePage()),
+  //         );
+  //       }
+  //     }
+  //   } catch (e) {
+  //     Fluttertoast.showToast(msg: e.toString());
+  //   }
+  // }
 }
